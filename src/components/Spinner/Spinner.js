1648a2fa41b6bbd925be83ce5ca4383ce590a6a6
Ozge Sari <ozgesari@Ozges-MacBook-Pro.local>
2024-01-21 21:44:03 +0000

    first commit

---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Spinner.css';
+
+export const Spinner = () => {
+    return (
+        <div className="spinner">
+        </div>
+    )
+}
+
+export default Spinner;
---
+++ src/components/WordLadder/WordLadder.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Spinner from '../Spinner/Spinner'
+
+const WordLadder = () => {
+    return (
+        <div className='wordLadder'>
+            <Spinner />
+        </div>
+    )
+}
+
+export default WordLadder
